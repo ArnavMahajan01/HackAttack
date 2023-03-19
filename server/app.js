23c8d05d0292687ab611d380a61993a333788a87
ArnavMahajan01 <arnav.mahajan.2001@gmail.com>
2023-03-19 23:06:33 +0530

    Mongodb Server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+
 const dotenv = require("dotenv");
-require('./connect')
 
 dotenv.config();
+
+require("./models/connect");
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
