2f2ef2a522cff55aacee485d70fef786387a4eca
ArnavMahajan01 <arnav.mahajan.2001@gmail.com>
2023-03-19 22:30:39 +0530

    Added environment

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: false, limit: "20mb" }));
+
+/* Initializing the path for routes */
+app.use("/", require("./routes"));
+
+PORT = process.env.PORT || 6000;
+const server = app.listen(PORT, function () {
+  console.log("This server port is up and running at PORT: " + PORT);
+});
