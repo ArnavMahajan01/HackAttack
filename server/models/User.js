46bfffef4a9e2eb8d23146d9e415198f5bead5ef
ayugupt <ayush.gupta.10301@gmail.com>
2023-03-20 00:23:29 +0530

    change user schema

--- server/models/User.js
+++ server/models/User.js
@@ -6,12 +6,43 @@ const stringValue = {
   require: true,
 };
 
+const SpecialUserSchema = new mongoose.Schema({
+  profileurl: {type: String, require: true},
+  dateofcreation: Date,
+  type: String,
+  dob: Date, 
+  gender: String
+})
+
+const LocationSchema = new mongoose.Schema({
+  latitude: {type: Number, required: true},
+  longitude: {type: Number, required: true}
+})
+
 const UserSchema = new mongoose.Schema(
   {
     name: stringValue,
     email: stringValue,
     password: stringValue,
     phone: stringValue,
+    data: SpecialUserSchema,
+    location: LocationSchema,
+    interaction: {
+      type: String,
+      required: true,
+      enum: ["phone", "chat", "video"],
+      default: "phone"
+    },
+    interactiontime: {
+      type: String,
+      required: true,
+      enum: ["morning", "afternoon", "evening"],
+    }, 
+    type: {
+      type: String,
+      required: true,
+      enum: ["volunteer", "organization", "beneficiary"],
+    }, 
   },
   {
     timestamps: true,
