1bace34c8fec685002980517ba654030353b42a2
ArnavMahajan01 <arnav.mahajan.2001@gmail.com>
2023-03-20 08:44:49 +0530

    Error in post js route line 54

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -1,74 +1,77 @@
-const Post = require('../models/Post')
-const PostJoin = require('../models/PostJoin')
+const Post = require("../models/Post");
+const PostJoin = require("../models/PostJoin");
 
-exports.createPost = async (req, res)=>{
-    const {userId, title, desc, date, typeofuser, orgdata} = req.body;
+exports.createPost = async (req, res) => {
+  const { userId, title, desc, date, typeofuser, orgdata } = req.body;
 
-
-    try{
-        let orginfo = {};
-        if(orgdata){
-            orginfo.orgdata = orgdata;
-            orginfo.orgdata.peoplejoined = 0;
-        }
-        if(typeofuser == "organization" && !orginfo){
-            return res.status(400).json({
-                error:"organization data not given for post"
-            })
-        }
-        const post = new Post({
-            userId: userId,
-            title: title,
-            desc: desc,
-            date: date,
-            typeofuser: typeofuser,
-            ...orginfo
-        })
-        await post.save();
-        res.status(201).json({
-            msg:"Successfully created post",
-            data: post.toJSON()
-        })
-    }catch(error){
-        res.status(400).json({
-            msg:"There was an error",
-            error: error
-        })
+  try {
+    let orginfo = {};
+    if (orgdata) {
+      orginfo.orgdata = orgdata;
+      orginfo.orgdata.peoplejoined = 0;
     }
-}
-
-exports.joinPost = async (req, res)=>{
-    const {userId, postId} = req.body;
-
-    try{
-        const joinDoc = await PostJoin.findOne({
-            userId: userId,
-            postId: postId
-        })
-
-        if(joinDoc){
-            throw "User already joined";
-        }
-        const post = Post.findOne({_id: postId});
-        if(post.maxpeople == post.peoplejoined){
-            throw "Max amount of people joined"
-        }
-
-        const newJoinDoc = new PostJoin({
-            userId: userId,
-            postId: postId
-        })
-        post.peoplejoined++;
-        await newJoinDoc.save();
-        await post.save();
-
-        res.status(200).json({
-            msg:"Added user"
-        })
-    }catch(error){
-        res.status(400).json({
-            msg:"There was an error",
-            error: error
-        })
+    if (typeofuser == "organization" && !orginfo) {
+      return res.status(400).json({
+        error: "organization data not given for post",
+      });
     }
-}
+    const post = new Post({
+      userId: userId,
+      title: title,
+      desc: desc,
+      date: date,
+      typeofuser: typeofuser,
+      ...orginfo,
+    });
+    await post.save();
+    res.status(201).json({
+      msg: "Successfully created post",
+      data: post.toJSON(),
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "There was an error",
+      error: error,
+    });
+  }
+};
+
+exports.joinPost = async (req, res) => {
+  const { userId, postId } = req.body;
+
+  try {
+    const joinDoc = await PostJoin.findOne({
+      userId: userId,
+      postId: postId,
+    });
+
+    if (joinDoc) {
+      throw "User already joined";
+    }
+
+    const post = Post.findOne({ _id: postId });
+
+    if (post.orgdata.maxpeople == post.orgdata.peoplejoined) {
+      throw "Max amount of people joined";
+    }
+    console.log("Inside try");
+    console.log("After max error");
+
+    const newJoinDoc = new PostJoin({
+      userId: userId,
+      postId: postId,
+    });
+    post.peoplejoined++;
+    await newJoinDoc.save();
+    await post.save();
+
+    res.status(200).json({
+      msg: "Added user",
+    });
+  } catch (error) {
+    res.status(400).json({
+      msg: "There was an error",
+      error: error,
+    });
+  }
+};
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const { auth } = require("../middleware/auth");
 const postController = require("../controller/postController");
 
 router.post("/createPost", auth, postController.createPost);
